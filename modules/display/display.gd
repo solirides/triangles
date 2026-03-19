670c8f7fc53bfb734b9ef99922498610952040e1
solirides <83032760+solirides@users.noreply.github.com>
2026-03-18 22:55:01 -0700

    misc

--- modules/display/display.gd
+++ modules/display/display.gd
@@ -1,11 +1,37 @@
 extends Node2D
 
 
+@export var player:Node = null
+
+var shapes = []
+var color = Color(1,0.5,0.5,1)
+var width = 4
+var draw_shape_progress = false
+var partial_shape = []
+
 func _ready() -> void:
 	queue_redraw()
 
 func _draw() -> void:
+	for shape in shapes:
+		match shape[0]:
+			"segment":
+				draw_line(shape[1], shape[2], color, width)
+			"circle":
+				pass
+			_:
+				pass
+	if len(partial_shape) >= 1 and draw_shape_progress == true:
+		match partial_shape[0]:
+			"segment":
+				
+			_:
+				pass
+				
 	draw_circle(Vector2(100,100), 40, Color(0.615, 0.979, 1.206, 1.0), false, 4)
 
 func _process(delta: float) -> void:
-	pass
+	queue_redraw()
+
+func shape_progress(data:Array):
+	partial_shape = data.duplicate()
--- modules/player/player.gd
+++ modules/player/player.gd
@@ -2,10 +2,29 @@ extends Node2D
 
 @export var accel = 1
 @export var pivot:Node = null
+@export var display:Node = null
 
 var projectile = preload("res://modules/projectile/projectile.tscn")
-
 var velocity = Vector2.ZERO
+enum CONSTRUCTION_STATE {
+	NONE,
+	SEGMENT_0,
+	SEGMENT_1,
+	SEGMENT_2,
+	CIRCLE_0,
+	CIRCLE_1,
+	CIRCLE_2
+}
+var construction_state = CONSTRUCTION_STATE.SEGMENT_0
+var current_shape = []
+var shapes = []
+var build_state = BUILD_STATE.NONE
+enum BUILD_STATE {
+	NONE,
+	PARTIAL,
+	COMPLETE
+}
+
 
 func _physics_process(delta: float) -> void:
 	var vec = Input.get_vector("move_left","move_right","move_up","move_down")
@@ -13,8 +32,44 @@ func _physics_process(delta: float) -> void:
 	velocity += vec * accel
 	velocity = lerp(velocity, Vector2.ZERO, 0.1)
 	self.position += velocity
+	#self.position += vec * 10
 	
+	if Input.is_action_just_pressed("primary"):
+		var a = projectile.instantiate()
+		get_tree().root.add_child(a)
+		a.position = self.position
+		a.linear_velocity = (get_global_mouse_position()-self.global_position).normalized()
+		a.linear_velocity *= 1000
+		a.look_at(get_global_mouse_position())
+	
+	if Input.is_action_just_pressed("construct"):
+		match construction_state:
+			CONSTRUCTION_STATE.SEGMENT_0:
+				current_shape.resize(3)
+				current_shape[0] = "segment"
+				current_shape[1] = self.global_position
+				construction_state = CONSTRUCTION_STATE.SEGMENT_1
+				build_state = BUILD_STATE.PARTIAL
+			CONSTRUCTION_STATE.SEGMENT_1:
+				current_shape[2] = self.global_position
+				write_shape(current_shape)
+				current_shape = []
+				construction_state = CONSTRUCTION_STATE.SEGMENT_0
+				BUILD_STATE.COMPLETE
+			CONSTRUCTION_STATE.SEGMENT_2:
+				pass
+			_:
+				pass
+		
 
 func _process(delta: float) -> void:
 	var pos = get_global_mouse_position()
 	pivot.look_at(pos)
+	if build_state == BUILD_STATE.PARTIAL:
+		display.draw_shape_progress = true
+		display.shape_progress(current_shape)
+	else:
+		display.draw_shape_progress = false
+
+func write_shape(data:Array):
+	display.shapes.append(data)
