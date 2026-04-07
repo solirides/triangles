f9e06d0372e613e8078d048d0d0df04369449d0f
solirides <83032760+solirides@users.noreply.github.com>
2026-04-06 18:04:40 -0700

    a whole lot of nothing

--- modules/game_manager/game_manager.gd
+++ modules/game_manager/game_manager.gd
@@ -6,7 +6,7 @@ var player:Node = null
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	get_window().grab_focus()
-	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
+	Input.set_mouse_mode(Input.MOUSE_MODE_CONFINED_HIDDEN)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -15,3 +15,10 @@ func _process(delta: float) -> void:
 
 func start_game():
 	get_tree().change_scene_to_file("res://scenes/world/world.tscn")
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("ui_cancel"):
+		if Input.mouse_mode != Input.MOUSE_MODE_VISIBLE:
+			Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+		else:
+			Input.set_mouse_mode(Input.MOUSE_MODE_CONFINED_HIDDEN)
--- scenes/room_map/room_map.gd
+++ scenes/room_map/room_map.gd
@@ -4,6 +4,11 @@ extends Node2D
 @export var max_width = 5
 @export var max_length = 10
 
+enum ROOM {
+	EMPTY,
+	ROOM,
+	PASSAGE
+}
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -17,22 +22,31 @@ func _process(delta: float) -> void:
 func generate_room():
 	var row = []
 	row.resize(max_width + 1)
-	row.fill("empty")
+	row.fill(ROOM.EMPTY)
 	
 	var rooms = []
 	rooms.resize(max_length + 1)
 	rooms.fill(row.duplicate())
 	
+	#var passages = rooms.duplicate()
+	
+	
 	# for each row
 	for x in range(2, max_length.size() - 1):
 		# for each cell
 		for y in range(1, max_width.size() - 1):
-			if rooms[x-1][y] != "empty":
+			if rooms[x-1][y] != ROOM.EMPTY:
 				# continue path
-				rooms[x][y] = "room1"
+				rooms[x][y] = ROOM.ROOM
 			else:
-				var neighbors = (rooms[x-1][y-1] != "empty") + (rooms[x-1][y+1] != "empty")
+				var neighbors = (rooms[x-1][y-1] != ROOM.EMPTY) + (rooms[x-1][y+1] != ROOM.EMPTY)
 				if randf() < neighbors * 0.3:
 					pass
+
+# 00r=r=r=r
+# 00=00000=
+# r=r=r=r=r=r
+# 0000=
+# 0000r=r=r=r
 
 #func get_neighbors(rooms:Array):
